6fee2c3c3418927ec99fe928d6e07f193eb4372a
Erik Demaine <edemaine@mit.edu>
2018-04-11 15:37:52 -0400

    Mobile config

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+  id: 'org.erikdemaine.coinsliding',
+  name: 'Coin Sliding',
+  version: '1.0.0',
+  author: 'Erik Demaine',
+  email: 'edemaine+coinsliding@mit.edu'
+});
+
+App.accessRule('*');
+App.accessRule('blob:*');
