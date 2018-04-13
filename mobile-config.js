21965ecb6b3a1e58a7647b200ce412dff3bbdebf
Erik Demaine <edemaine@mit.edu>
2018-04-13 00:53:48 -0400

    Mobile build instructions

--- mobile-config.js
+++ mobile-config.js
@@ -1,3 +1,8 @@
+// meteor build ..\build --server=https://coinsliding.erikdemaine.org/
+// cd ..\build\android\project\build\outputs\apk\release
+// jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 android-release-unsigned.apk coinsliding
+// zipalign -f 4 android-release-unsigned.apk coinsliding.apk
+
 App.info({
   id: 'org.erikdemaine.coinsliding',
   name: 'Coin Sliding',
